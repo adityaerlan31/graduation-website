dec7929fc4d5a965f4bb1bed48bacf9ce7d52ad0
adityaerlan31 <adityaerlan3112@gmail.com>
2026-06-12 15:26:24 +0700

    add scroll animation

--- script.js
+++ script.js
@@ -88,3 +88,35 @@ function createSakura(){
 }
 
 setInterval(createSakura,500);
+
+function revealOnScroll(){
+
+    const reveals =
+    document.querySelectorAll(".reveal");
+
+    reveals.forEach(section => {
+
+        const windowHeight =
+        window.innerHeight;
+
+        const revealTop =
+        section.getBoundingClientRect().top;
+
+        const revealPoint = 100;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener(
+    "scroll",
+    revealOnScroll
+);
+
+revealOnScroll();
